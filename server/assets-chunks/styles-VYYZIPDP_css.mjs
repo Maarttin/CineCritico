2d2209d790370e9b11cbaa5620a15a2fbccc4b85
Martín <martinaragonsanchez@gmail.com>
2025-09-04 16:06:59 -0600

    Auto-generated commit

--- server/assets-chunks/styles-VYYZIPDP_css.mjs
+++
@@ -1,2 +0,0 @@
-export default `body{margin:0;padding:0;display:flex;justify-content:center;align-items:flex-start;min-height:100vh;background-color:#c8e0f5}
-`;
--- server/chunk-FVTTCWXS.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-var e={firebase:{apiKey:"AIzaSyCqn1RRNXi6NaKsVvv8bxacfD3cWGNpOcA",authDomain:"cinecritico-70e42.firebaseapp.com",projectId:"cinecritico-70e42",storageBucket:"cinecritico-70e42.appspot.com",messagingSenderId:"385956676789",appId:"1:385956676789:web:e50efbebd9b2edb8a46afc"},tmdbApiKey:"100d9693bc432e6bcf8d3dbffff91466"};export{e as a};
